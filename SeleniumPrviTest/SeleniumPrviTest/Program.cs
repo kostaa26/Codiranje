f0985e079123d52553b8e28abb7986b6dd7bbd14
QA36 <QA36@DESKTOP-AESHFBQ>
2022-11-10 21:09:48 +0100

    Selenium

---
+++ SeleniumPrviTest/SeleniumPrviTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumPrviTest
+{
+    public class Program
+
+    {
+        IWebDriver drive = new ChromeDriver();
+        static void Main(string[] args)
+        {
+            
+
+        }
+        [SetUp]
+        public void Inicijalizacija()
+        {
+            drive.Navigate().GoToUrl("https://www.saucedemo.com/");
+            drive.Manage().Window.Maximize();
+        }
+        [Test]
+        public void Test1()
+        {
+            IWebElement userName = drive.FindElement(By.Id("user-name"));
+            userName.SendKeys("standard_user");
+
+            IWebElement password = drive.FindElement(By.Id("password"));
+            password.SendKeys("secret_sauce");
+
+            IWebElement LoginButton = drive.FindElement(By.Id("login-button"));
+            LoginButton.Submit();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            drive.Close();
+        }
+    }
+}
